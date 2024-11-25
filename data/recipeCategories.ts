3208accc22cdf081f9c05541093375dfb6fe016d
Chase Mortensen <chase@chase-mortensen.dev>
2024-11-25 09:08:42 -0700

    Initial commit

---
+++ data/recipeCategories.ts
@@ -0,0 +1,9 @@
+export const CATEGORIES = {
+  BREAKFAST: 'Breakfast',
+  ENTREES: 'Entrées',
+  PASTAS: 'Pastas',
+  SALADS: 'Salads',
+  SANDWICHES: 'Sandwiches',
+  SNACKS: 'Snacks and Desserts',
+  SOUPS: 'Soups'
+}
